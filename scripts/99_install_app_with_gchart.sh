94995a8e22630a58b0afb88dc50cc325eb50e7be
Michael Dundek <mdundek@gmail.com>
2022-02-28 20:37:53 +0100

    update

---
+++ scripts/99_install_app_with_gchart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl create ns nginx
+
+helm upgrade --install nginx-test ./generic-helm-chart \
+  --values ./generic-helm-chart/nginx_values.yaml \
+  --kubeconfig /etc/rancher/k3s/k3s.yaml \
+  -n nginx
