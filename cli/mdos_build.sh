49248b295411986878a88b1d28ca28ea2326a487
Michael Dundek <mdundek@gmail.com>
2022-07-12 10:24:11 +0000

    updates

--- cli/mdos_build.sh
+++ cli/mdos_build.sh
@@ -10,6 +10,34 @@ source .env
 source ./lib/components.sh
 source ./lib/helpers.sh
 
+shift
+while [ "$1" != "" ]; do
+    case $1 in
+        --all|-a )
+            shift
+            BUILD_ALL=1
+        ;; 
+        * ) echo "Invalid parameter detected => $1"
+            exit 1
+    esac
+    shift
+done
+
+# ------------------------------------------------
+build_component() {
+    if [ -f ./pre_build.sh ]; then
+        chmod a+x ./pre_build.sh
+        ./pre_build.sh
+    fi
+
+    COMP_NAME=$(basename "$(pwd)")
+
+    I_REPO=$(yq eval '.appComponents[] | select(.name == "'"$COMP_NAME"'") | .image.repository' ../values.yaml)
+    I_TAG=$(yq eval '.appComponents[] | select(.name == "'"$COMP_NAME"'") | .image.tag' ../values.yaml)
+
+    docker build -t $REGISTRY_HOST/$I_REPO:$I_TAG .
+    docker push $REGISTRY_HOST/$I_REPO:$I_TAG
+}
 
 # ################################################
 # ############ TRY CATCH INTERCEPTORS ############
@@ -34,29 +62,39 @@ source ./lib/helpers.sh
     # ############### EXECUTE ################
     cd $CDIR
 
-    if [ ! -f ./Dockerfile ]; then
-        error "You do not seem to be in a application component directory"
-        exit 1
+    ## Build current component only
+    if [ -z $BUILD_ALL ]; then
+        if [ ! -f ./Dockerfile ]; then
+            error "You do not seem to be in a application component directory"
+            exit 1
+        fi
+
+        if [ ! -f ../values.yaml ]; then
+            error "You do not seem to be in a MDOS project"
+            exit 1
+        fi
+
+        build_component
+    else # Build all components of application
+        if [ -f ../values.yaml ]; then
+            cd ..
+        fi
+
+        if [ ! -f ./values.yaml ]; then
+            error "You do not seem to be in a MDOS project"
+            exit 1
+        fi
+
+        BASE_D=$(pwd)
+
+        for component in */ ; do
+            if [ -f $BASE_D/$component/Dockerfile ]; then
+                cd $BASE_D/$component
+                build_component
+            fi
+        done
     fi
-
-    if [ ! -f ../values.yaml ]; then
-        error "You do not seem to be in a MDOS project"
-        exit 1
-    fi
-
-    if [ -f ./pre_build.sh ]; then
-        chmod a+x ./pre_build.sh
-        ./pre_build.sh
-    fi
-
-    COMP_NAME=$(basename "$(pwd)")
-
-    I_REPO=$(yq eval '.appComponents[] | select(.name == "'"$COMP_NAME"'") | .image.repository' ../values.yaml)
-    I_TAG=$(yq eval '.appComponents[] | select(.name == "'"$COMP_NAME"'") | .image.tag' ../values.yaml)
-
-    docker build -t $REGISTRY_HOST/$I_REPO:$I_TAG .
-    docker push $REGISTRY_HOST/$I_REPO:$I_TAG
 
     echo ""
-    info "Application build successfully"
+    info "Application(s) build successfully"
 )
