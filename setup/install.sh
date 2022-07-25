337dd4190fd50518dce44e9fd009873fd5ee50a3
Michael Dundek <mdundek@gmail.com>
2022-07-25 22:34:36 +0200

    refactor

--- setup/install.sh
+++ setup/install.sh
@@ -410,15 +410,16 @@ DNS.2 = *.$DOMAIN" > $SSL_ROOT/config.cfg
 # ############################################
 install_k3s() {
     curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-backend=none --cluster-cidr=192.169.0.0/16 --disable-network-policy --disable=traefik --write-kubeconfig-mode=664" sh - &>> $LOG_FILE
-    # Install Calico
-    kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml &>> $LOG_FILE
-    kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml &>> $LOG_FILE
-
+    
     # Configure user K8S credentiald config file
     mkdir -p $HOME/.kube
     rm -rf $HOME/.kube/config
     cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
     chmod 600 $HOME/.kube/config
+
+    # Install Calico
+    kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml &>> $LOG_FILE
+    kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml &>> $LOG_FILE
 
     info "Waiting for kubernetes to become ready..."
     ATTEMPTS=0
