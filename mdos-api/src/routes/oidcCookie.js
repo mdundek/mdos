4db88c17621194f209106666c346a1e4cc706280
Michael Dundek <mdundek@gmail.com>
2022-08-11 11:30:46 +0000

    added auth to all backend

--- mdos-api/src/routes/oidcCookie.js
+++ mdos-api/src/routes/oidcCookie.js
@@ -9,41 +9,28 @@ module.exports = (app, req, res) => {
                 res.status(503).send("Keycloak is not installed");
                 return;
             }
+            const list = {}
+            const cookieHeader = req.headers?.cookie
+            if (!cookieHeader) {
+                res.status(403).send("Not athenticated");
+                return;
+            }
 
-            // if (!req.headers['x-auth-request-access-token']) {
-            //     res.status(403).send("Not athenticated");
-            //     return;
-            // }
+            cookieHeader.split(`;`).forEach(function (cookie) {
+                let [name, ...rest] = cookie.split(`=`)
+                name = name?.trim()
+                if (!name) return
+                const value = rest.join(`=`).trim()
+                if (!value) return
+                list[name] = decodeURIComponent(value)
+            })
 
-            // let jwtToken = jwt_decode(req.headers['x-auth-request-access-token'])
-
-            // if (jwtToken.resource_access.mdos && jwtToken.resource_access.mdos.roles.find((r) => r == 'mdos_admin')) {
-                const list = {}
-                const cookieHeader = req.headers?.cookie
-                if (!cookieHeader) {
-                    res.status(403).send("Not athenticated");
-                    return;
-                }
-
-                cookieHeader.split(`;`).forEach(function (cookie) {
-                    let [name, ...rest] = cookie.split(`=`)
-                    name = name?.trim()
-                    if (!name) return
-                    const value = rest.join(`=`).trim()
-                    if (!value) return
-                    list[name] = decodeURIComponent(value)
-                })
-
-                if(!list["_oauth2_proxy"]) {
-                    res.status(403).send("Not athenticated");
-                    return;
-                } else {
-                    let template = fs.readFileSync(path.join(APP_ROOT, 'assets', 'jwt.html'), 'utf8')
-                    res.send(template.replace('{TOKEN}', list["_oauth2_proxy"]))
-                }
-            // } else {
-            //     res.status(403).send("Not authorized");
-            //     return;
-            // }
+            if(!list["_oauth2_proxy"]) {
+                res.status(403).send("Not athenticated");
+                return;
+            } else {
+                let template = fs.readFileSync(path.join(APP_ROOT, 'assets', 'jwt.html'), 'utf8')
+                res.send(template.replace('{TOKEN}', list["_oauth2_proxy"]))
+            }
         })
 }
