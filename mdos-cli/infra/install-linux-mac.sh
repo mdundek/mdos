021416649d3fa4e6c69b67d29ac33d5d3c0d3470
Michael Dundek <mdundek@gmail.com>
2022-09-19 18:39:30 +0200

    Bumped project up to version 1.1.0

--- mdos-cli/infra/install-linux-mac.sh
+++ mdos-cli/infra/install-linux-mac.sh
@@ -16,7 +16,7 @@
     $SUDO bash <<SCRIPT
 set -e
 
-CLI_VERSION=v1.0.18
+CLI_VERSION=v1.1.0
 
 if [[ ! ":\$PATH:" == *":/usr/local/bin:"* ]]; then
     echo "Your path is missing /usr/local/bin, you need to add this to use this installer."
