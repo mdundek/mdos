c4099305d1f3efc722c7bb8b5284109c31569728
Michael Dundek <mdundek@gmail.com>
2022-09-19 17:01:03 +0200

    refactor

--- mdos-cli/infra/create-release.sh
+++ mdos-cli/infra/create-release.sh
@@ -138,309 +138,6 @@ get_parent_branch() {
     eval $__resultvar="$G_PARENT"
 }
 
-# ######################################
-# ############### RELEASE ##############
-# ######################################
-release_from_main() {
-    echo ""
-    # select_repos SEL_REPOS --mandatory --include-stack-chart
-
-    # # First, we make sure all target repos have no un-committed stuff as a pre-flight check
-    # info "Repo pre-flight checks..."
-    
-    # # Get target repo absolute path
-    # cd $_DIR & cd ..
-    
-    # REPO_BRANCH_MDOS=$(get_current_branch)
-    # REPO_DIR=$(pwd)
-
-    # PROSCEED_IF_DIRTY=""
-    # if [ "$REPO_BRANCH_MDOS" == "main" ]; then
-    #     check_if_git_is_clean PROSCEED_IF_DIRTY strict
-    # else
-    #     check_if_git_is_clean PROSCEED_IF_DIRTY
-    #     git checkout main > /dev/null 2>&1
-    # fi
-    # git_pull_rebase PROSCEED_IF_DIRTY strict
-
-    # git rev-parse --verify release > /dev/null 2>&1
-    # if [ $? != 0 ]; then
-    #     git checkout -b release > /dev/null 2>&1
-    #     git push -u origin release > /dev/null 2>&1
-    # else
-    #     git checkout release > /dev/null 2>&1
-    #     git_pull_rebase PROSCEED_IF_DIRTY strict
-    # fi
-
-    # git checkout main > /dev/null 2>&1
-   
-    # # Version bump target
-    # question "What version upgrade type do you want to do for those repos?"
-    # OPTIONS_VALUES=("major" "feature" "bug")
-    # OPTIONS_LABELS=("X.y.z" "x.Y.z" "x.y.Z")
-    # OPTIONS_STRING=""
-    # for y in "${!OPTIONS_VALUES[@]}"; do
-    #     OPTIONS_STRING+="${OPTIONS_VALUES[$y]} (${OPTIONS_LABELS[$y]});"
-    # done
-    # prompt_for_select VERSION_BUMP_FLAGS "$OPTIONS_STRING"
-    # for y in "${!VERSION_BUMP_FLAGS[@]}"; do
-    #     if [ "${VERSION_BUMP_FLAGS[$y]}" == "true" ]; then
-    #         VERSION_BUMP_TARGET="${OPTIONS_VALUES[$y]}"
-    #     fi
-    # done
-
-    # # Now create release merges
-    # process_repo_release() {
-        
-    #     C_PWD=$(pwd)
-    #     REPO_DIR=$(basename $C_PWD)
-        
-    #     # Version bump
-    #     bump_and_merge() {
-    #         (
-    #             ./infra/version_bump.sh --type $1 && \
-    #             git checkout release > /dev/null 2>&1 && \
-    #             git merge --no-ff main > /dev/null 2>&1
-    #             git push origin release > /dev/null 2>&1
-    #         ) || ( exit 1 )
-    #     }
-    #     return_to_branch() {
-    #         git checkout $REPO_BRANCH_MDOS > /dev/null 2>&1
-    #     }
-    #     on_error() {
-    #         error "$1. You should manually clean up and fix potential inconcistencies."
-    #         return_to_branch
-    #         exit 1
-    #     }
-    #     info "Bump up version & merge to branch \"release\"..."
-    #     bump_and_merge $VERSION_BUMP_TARGET || on_error "Could not create release for repo ${c_warn}$REPO_DIR${c_reset}"
-        
-    #     return_to_branch
-
-    #     info "Successfully merged repo ${c_warn}$REPO_DIR${c_reset} to release branch on version ${c_warn}$CURRENT_APP_VERSION${c_reset}"
-    # }
-
-    # info "Processing Repo..."
-    # process_repo_release $_PATH $_CHART_PATH
-}
-
-# ######################################
-# ########### TAG & PUBLISH ############
-# ######################################
-tag_publish() {
-    echo ""
-    # # First, we make sure all target repos have no un-committed stuff as a pre-flight check
-    # info "Repo pre-flight checks..."
-    # for i in "${!SEL_REPOS[@]}"; do
-    #     # Get target repo absolute path
-    #     cd $_DIR
-    #     case ${SEL_REPOS[$i]} in
-    #         okube) 
-    #             _PATH=../../scds-bf74-okube-api
-    #             cd $_PATH && REPO_BRANCH_MDOS=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #         ms) 
-    #             _PATH=../../scds-bf74-osp-ms-jobs
-    #             cd $_PATH && REPO_BRANCH_MS=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #         broker-client) 
-    #             _PATH=../../scds-bf74-broker-client
-    #             cd $_PATH && REPO_BRANCH_BROKER_CLIENT=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #         osp-logger) 
-    #             _PATH=../../scds-bf74-npm-logger
-    #             cd $_PATH && REPO_BRANCH_OSP_LOGGER=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #         helm-chart) 
-    #             _PATH=../../scds-bf74-generic-helm-chart-stateless
-    #             cd $_PATH && REPO_BRANCH_GEN_HELM=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #         cc) 
-    #             _PATH=../../scds-bf74-osp-control-center
-    #             cd $_PATH && REPO_BRANCH_CC=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #         stack) 
-    #             _PATH=../
-    #             cd $_PATH && REPO_BRANCH_STACK=$(get_current_branch) && cd $_DIR
-    #         ;;
-    #     esac
-        
-    #     if [ ! -d "$_PATH" ]; then
-    #         error "Git repo for project \"${SEL_REPOS[$i]}\" does not exist. Clone it next to the repo \"scds-bf74-scds-stack-helm-chart\" and try again."
-    #         exit 1
-    #     fi
-    #     cd $_PATH
-
-    #     git rev-parse --verify release > /dev/null 2>&1
-    #     if [ $? != 0 ]; then
-    #         error "There is no release branch for this repo."
-    #         exit 1
-    #     fi
-
-    #     C_PWD=$(pwd)
-    #     REPO_DIR=$(basename $C_PWD)
-    #     case ${SEL_REPOS[$i]} in
-    #         okube) _CHART_PATH=./infra/helm/scds-osp-okube-api/Chart.yaml;;
-    #         ms) _CHART_PATH=./infra/helm/scds-osp-ms-deploy/Chart.yaml;;
-    #         broker-client) _CHART_PATH=./package.json;;
-    #         osp-logger) _CHART_PATH=./package.json;;
-    #         helm-chart) _CHART_PATH=./Chart.yaml;;
-    #         cc) _CHART_PATH=./infra/helm/scds-osp-cc/Chart.yaml;;
-    #         stack) get_full_path _CHART_PATH ./chart/Chart.yaml;;
-    #     esac
-      
-    #     if [ "$REPO_DIR" == "scds-bf74-broker-client" ] || [ "$REPO_DIR" == "scds-bf74-npm-logger" ]; then
-    #         CURRENT_APP_VERSION=$(cat $_CHART_PATH | grep "\"version\":" | cut -d ":" -f2 | cut -d'"' -f 2)
-    #     else
-    #         CURRENT_APP_VERSION=$(cat $_CHART_PATH | grep 'version:' | head -1 | cut -d ":" -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-    #     fi
-
-    #     GIT_TAG_EXISTS=$(git tag -l "v$CURRENT_APP_VERSION")
-    #     if [ ! -z $GIT_TAG_EXISTS ]; then
-    #         error "The tag \"v$CURRENT_APP_VERSION\" already exists for repo \"$REPO_DIR\"."
-    #         exit 1
-    #     fi
-
-    #     REPO_BRANCH=$(get_current_branch)
-    #     PROSCEED_IF_DIRTY=""
-    #     if [ "$REPO_BRANCH" == "main" ]; then
-    #         check_if_git_is_clean PROSCEED_IF_DIRTY strict
-    #     else
-    #         check_if_git_is_clean PROSCEED_IF_DIRTY
-    #         git checkout main > /dev/null 2>&1
-    #     fi
-
-    #     git_pull_rebase PROSCEED_IF_DIRTY strict
-
-    #     git checkout release > /dev/null 2>&1
-    # done
-
-
-
-
-
-
-
-
-    # # Now create tag & publish
-    # process_repo_tag_publish() {
-    #     cd $1
-
-    #     C_PWD=$(pwd)
-    #     REPO_DIR=$(basename $C_PWD)
-        
-    #     tag() {
-    #         (
-    #             git tag -a v$1 -m "Release version v$1" > /dev/null 2>&1 && \
-    #             git push origin --tags > /dev/null 2>&1
-    #         ) || ( exit 1 )
-    #     }
-    #     return_to_branch() {
-    #         if [ "$REPO_DIR" == "scds-bf74-okube-api" ]; then
-    #             git checkout $REPO_BRANCH_MDOS > /dev/null 2>&1
-    #         elif [ "$REPO_DIR" == "scds-bf74-osp-ms-jobs" ]; then
-    #             git checkout $REPO_BRANCH_MS > /dev/null 2>&1
-    #         elif [ "$REPO_DIR" == "scds-bf74-broker-client" ]; then
-    #             git checkout $REPO_BRANCH_BROKER_CLIENT > /dev/null 2>&1
-    #         elif [ "$REPO_DIR" == "scds-bf74-npm-logger" ]; then
-    #             git checkout $REPO_BRANCH_OSP_LOGGER > /dev/null 2>&1
-    #         elif [ "$REPO_DIR" == "scds-bf74-generic-helm-chart-stateless" ]; then
-    #             git checkout $REPO_BRANCH_GEN_HELM > /dev/null 2>&1
-    #         elif [ "$REPO_DIR" == "scds-bf74-osp-control-center" ]; then
-    #             git checkout $REPO_BRANCH_CC > /dev/null 2>&1
-    #         elif [ "$REPO_DIR" == "scds-bf74-scds-stack-helm-chart" ]; then
-    #             git checkout $REPO_BRANCH_STACK > /dev/null 2>&1
-    #         fi
-    #     }
-    #     on_error() {
-    #         error "$1. You should manually clean up and fix potential inconcistencies."
-    #         return_to_branch
-    #         exit 1
-    #     }
-    
-    #     if [ "$REPO_DIR" == "scds-bf74-broker-client" ] || [ "$REPO_DIR" == "scds-bf74-npm-logger" ]; then
-    #         CURRENT_APP_VERSION=$(cat $2 | grep "\"version\":" | cut -d ":" -f2 | cut -d'"' -f 2)
-    #     else
-    #         CURRENT_APP_VERSION=$(cat $2 | grep 'version:' | head -1 | cut -d ":" -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-    #     fi
-        
-    #     info "Tagging current commit with version $CURRENT_APP_VERSION..."
-    #     tag $CURRENT_APP_VERSION || on_error "Could not tag commit for repo ${c_warn}$REPO_DIR${c_reset}"
-
-    #     return_to_branch
-
-    #     info "Successfully tagged repo ${c_warn}$REPO_DIR${c_reset} on release branch: version ${c_warn}$CURRENT_APP_VERSION${c_reset}"
-    # }
-
-
-
-
-
-
-    # # Get previous tag name for repo
-    # get_previous_tag() {
-    #     local __resultvar=$1
-    #     readarray -t LAST_TWO_TAGS < <( git tag | sort -V | tail -2 )
-    #     if [ "${#LAST_TWO_TAGS[@]}" -eq "2" ]; then
-    #         eval $__resultvar='${LAST_TWO_TAGS[0]}'
-    #     else   
-    #         eval $__resultvar=''
-    #     fi
-    # }
-
-    # # Create release with releasenotes
-    # git_release() {
-    #     TAG_NAME="$1"
-
-    #     # Login to Github using gh CLI
-    #     echo "$GITHUB_TOKEN" > .githubtoken
-    #     gh auth login --hostname github.airbus.corp --git-protocol https --with-token < .githubtoken
-    #     rm -rf .githubtoken
-
-    #     # Create release
-    #     gh release create $TAG_NAME -F ./changelogs/$TAG_NAME.md
-    # }
-
-    # # First all but stack
-    # for i in "${!SEL_REPOS[@]}"; do
-    #     # Get target repo absolute path
-    #     cd $_DIR
-    #     TARGET_REPO_KEY=${SEL_REPOS[$i]}
-    #     unset _PATH
-    #     case $TARGET_REPO_KEY in
-    #         okube) _PATH=../../scds-bf74-okube-api;;
-    #         ms) _PATH=../../scds-bf74-osp-ms-jobs;;
-    #         broker-client) _PATH=../../scds-bf74-broker-client;;
-    #         osp-logger) _PATH=../../scds-bf74-npm-logger;;
-    #         helm-chart) _PATH=../../scds-bf74-generic-helm-chart-stateless;;
-    #         cc) _PATH=../../scds-bf74-osp-control-center;;
-    #         stack) _PATH=../;;
-    #     esac
-
-    #     unset _CHART_PATH
-    #     case $TARGET_REPO_KEY in
-    #         okube) _CHART_PATH=./infra/helm/scds-osp-okube-api/Chart.yaml;;
-    #         ms) _CHART_PATH=./infra/helm/scds-osp-ms-deploy/Chart.yaml;;
-    #         broker-client) _CHART_PATH=./package.json;;
-    #         osp-logger) _CHART_PATH=./package.json;;
-    #         helm-chart) _CHART_PATH=./Chart.yaml;;
-    #         cc) _CHART_PATH=./infra/helm/scds-osp-cc/Chart.yaml;;
-    #         stack) get_full_path _CHART_PATH ../chart/Chart.yaml;;
-    #     esac
-    #     process_repo_tag_publish $_PATH $_CHART_PATH
-        
-    #     # Now create release and release notes for this new tag
-    #     CURRENT_TAG_NAME="$(git tag | sort -V | tail -1)"
-    #     if [ "$CURRENT_TAG_NAME" != "" ]; then
-    #         info "Generating changelog file for tag $CURRENT_TAG_NAME..."
-    #         get_previous_tag LAST_TAG_NAME
-           
-    #         info "Creating new Git release..."
-    #         RELEASE_URL=$(git_release $CURRENT_TAG_NAME)
-    #         info "Release created for tag $CURRENT_TAG_NAME: $RELEASE_URL"
-    #     fi
-    # done
-}
 
 # ######################################
 # ############### MAIN #################
@@ -478,26 +175,26 @@ tag_publish() {
     fi
 
     # Check git status
-    # check_if_git_has_unstaiged_changes UNSTAGED_CHANGES "strict"
-    # PROSCEED_IF_DIRTY=""
-    # if [ "$REPO_BRANCH_MDOS" == "main" ]; then
-    #     check_if_git_is_clean PROSCEED_IF_DIRTY strict
-    # else
-    #     check_if_git_is_clean PROSCEED_IF_DIRTY
-    #     git checkout main > /dev/null 2>&1
-    # fi
-    # git_pull_rebase PROSCEED_IF_DIRTY strict
+    check_if_git_has_unstaiged_changes UNSTAGED_CHANGES "strict"
+    PROSCEED_IF_DIRTY=""
+    if [ "$REPO_BRANCH_MDOS" == "main" ]; then
+        check_if_git_is_clean PROSCEED_IF_DIRTY strict
+    else
+        check_if_git_is_clean PROSCEED_IF_DIRTY
+        git checkout main > /dev/null 2>&1
+    fi
+    git_pull_rebase PROSCEED_IF_DIRTY strict
 
-    # git rev-parse --verify release > /dev/null 2>&1
-    # if [ $? != 0 ]; then
-    #     git checkout -b release > /dev/null 2>&1
-    #     git push -u origin release > /dev/null 2>&1
-    # else
-    #     git checkout release > /dev/null 2>&1
-    #     git_pull_rebase PROSCEED_IF_DIRTY strict
-    # fi
+    git rev-parse --verify release > /dev/null 2>&1
+    if [ $? != 0 ]; then
+        git checkout -b release > /dev/null 2>&1
+        git push -u origin release > /dev/null 2>&1
+    else
+        git checkout release > /dev/null 2>&1
+        git_pull_rebase PROSCEED_IF_DIRTY strict
+    fi
 
-    # git checkout main > /dev/null 2>&1
+    git checkout main > /dev/null 2>&1
 
     # Determine new version
     question "What version upgrade type do you want to do for those repos?"
@@ -576,6 +273,28 @@ tag_publish() {
 
     # Now we create the release for this tag
     # Create release with releasenotes
+    generate_release_files() {
+        # Package files
+        cd ./mdos-cli
+        npm run package
+
+        # Rename files
+        cd ./dist
+        for f in ./*.tar.*; do
+            if [[ $f == *"darwin-arm64.tar.gz"* ]]; then
+                AUID=$f
+                AUID=${AUID##*mdos-v1.0.13-}
+                AUID=${AUID%%-darwin-arm64.tar.gz*}
+            fi
+        done
+        if [ ! -z $AUID ]; then
+            for f in ./*.tar.*; do
+                mv $f ${f/-$AUID/}   
+            done
+        fi
+        cd ../..
+    }
+
     git_release() {
         TAG_NAME="$1"
 
@@ -585,7 +304,7 @@ tag_publish() {
         rm -rf .githubtoken
 
         # Create release
-        gh release create --generate-notes --target release --prerelease --title "MDos Platform release v$CURRENT_APP_VERSION" $TAG_NAME ./mdos-cli/dist/*.tar.*
+        gh release create --generate-notes --target release $TAG_NAME ./mdos-cli/dist/*.tar.*
     }
 
     if [ -z $GITHUB_TOKEN ]; then
@@ -593,9 +312,14 @@ tag_publish() {
         echo "GITHUB_TOKEN=$GITHUB_TOKEN" >> $_DIR/.env
     fi
 
-    # RELEASE_URL=$(git_release $CURRENT_APP_VERSION)
-    # info "Release created for tag $CURRENT_APP_VERSION: $RELEASE_URL"
+    # Generate assets
+    generate_release_files
+    RELEASE_URL=$(git_release $CURRENT_APP_VERSION)
 
+    # Clean up
+    rm -rf ./mdos-cli/dist/*.tar.*
+
+    info "Release created for tag $CURRENT_APP_VERSION: $RELEASE_URL"
 )
 
 
