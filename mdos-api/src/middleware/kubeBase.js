9b4e28a0f2d74017300ffa6e4c6c8f276dd1c4b1
Michael Dundek <mdundek@gmail.com>
2022-10-23 17:14:43 +0200

    refactor

--- mdos-api/src/middleware/kubeBase.js
+++ mdos-api/src/middleware/kubeBase.js
@@ -320,7 +320,7 @@ class KubeBase extends KubeBaseConstants {
 
             if(issuerName) {
                 const resOneIssuers = await axios.get(new URL(`https://${this.K3S_API_SERVER}/apis/cert-manager.io/v1/namespaces/${namespaceName}/issuers/${issuerName}`).href, this.k8sAxiosHeader)
-                console.log(JSON.stringify(resOneIssuers, null, 4))
+                console.log(JSON.stringify(resOneIssuers.data, null, 4))
             }
             
 
