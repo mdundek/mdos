40dd06212e0035ac2b5a050c2988db2df433abc3
Michael Dundek <mdundek@gmail.com>
2022-10-27 10:31:22 +0200

    refactor

--- mdos-api/src/services/mdos/mdos.class.js
+++ mdos-api/src/services/mdos/mdos.class.js
@@ -41,43 +41,43 @@ exports.Mdos = class Mdos extends MdosCore {
         console.log(new Date().getTime(), data)
         if (data.type == 'deploy') {
             // Parse values file
-            let valuesYaml = YAML.parse(Buffer.from(data.values, 'base64').toString('ascii'))
+        //     let valuesYaml = YAML.parse(Buffer.from(data.values, 'base64').toString('ascii'))
 
-            // Ensure namespace is ready
-            await this.validateNamespaceForDeployment(valuesYaml)
+        //     // Ensure namespace is ready
+        //     await this.validateNamespaceForDeployment(valuesYaml)
 
-            // Make sure we have at least one component
-            if (!valuesYaml.components || valuesYaml.components.length == 0) {
-                throw new BadRequest('ERROR: Application has no components')
-            }
+        //     // Make sure we have at least one component
+        //     if (!valuesYaml.components || valuesYaml.components.length == 0) {
+        //         throw new BadRequest('ERROR: Application has no components')
+        //     }
 
-            // Validate app schema
-            if (!valuesYaml.schemaVersion || typeof valuesYaml.schemaVersion != 'string') {
-                throw new BadRequest('ERROR: Missing schema version in your manifest (expected property: schemaVersion)')
-            }
-            const validationErrors = this.app.get('schemaValidator')[valuesYaml.schemaVersion].instance.validate(valuesYaml)
-            if (validationErrors.length > 0) {
-                throw new BadRequest(validationErrors.map((e) => e.stack).join('\n'))
-            }
+        //     // Validate app schema
+        //     if (!valuesYaml.schemaVersion || typeof valuesYaml.schemaVersion != 'string') {
+        //         throw new BadRequest('ERROR: Missing schema version in your manifest (expected property: schemaVersion)')
+        //     }
+        //     const validationErrors = this.app.get('schemaValidator')[valuesYaml.schemaVersion].instance.validate(valuesYaml)
+        //     if (validationErrors.length > 0) {
+        //         throw new BadRequest(validationErrors.map((e) => e.stack).join('\n'))
+        //     }
 
-            // Enrich values data
-            valuesYaml = await this.enrichValuesForDeployment(valuesYaml)
+        //     // Enrich values data
+        //     valuesYaml = await this.enrichValuesForDeployment(valuesYaml)
 
-            // If we need to make sure that the pod gets restarted if already deployed because of a volume sync change
-            if (data.restart) {
-                valuesYaml.forceUpdate = true
-            }
+        //     // If we need to make sure that the pod gets restarted if already deployed because of a volume sync change
+        //     if (data.restart) {
+        //         valuesYaml.forceUpdate = true
+        //     }
 
-            // Deploy
-            try {
-                await this.app.get('kube').mdosGenericHelmInstall(valuesYaml.tenantName, valuesYaml, data.processId)
-            } catch (helmError) {
-                if (Array.isArray(helmError)) throw new GeneralError('ERROR: ' + helmError.join('\n'))
-                else throw helmError
-            }
-        } else {
-            throw new BadRequest('ERROR: Malformed API request')
-        }
+        //     // Deploy
+        //     try {
+        //         await this.app.get('kube').mdosGenericHelmInstall(valuesYaml.tenantName, valuesYaml, data.processId)
+        //     } catch (helmError) {
+        //         if (Array.isArray(helmError)) throw new GeneralError('ERROR: ' + helmError.join('\n'))
+        //         else throw helmError
+        //     }
+        // } else {
+        //     throw new BadRequest('ERROR: Malformed API request')
+        // }
         return data
     }
 }
