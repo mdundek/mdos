2d43028b23642fcdd159aee7eeeb024c51ba5a16
Michael Dundek <mdundek@gmail.com>
2022-10-27 08:56:06 +0200

    refactor

--- mdos-api/src/libs/terminal.js
+++ mdos-api/src/libs/terminal.js
@@ -9,6 +9,11 @@ const terminalCommand = async (command, jsonResponse) => {
     return new Promise((resolve, reject) => {
         try {
             shell.exec(command, { silent: true }, function (code, stdout, stderr) {
+                console.log("-------**")
+                console.log(code)
+                console.log(stdout)
+                console.log(stderr)
+                console.log("-------**")
                 if (code == 0) {
                     if (jsonResponse) {
                         resolve(JSON.parse(stdout.split('\n').filter((o) => o.length > 0)))
