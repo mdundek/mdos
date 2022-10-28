56229d4f11113f67dc8c0ddc5a63319a1f5a6c87
Michael Dundek <mdundek@gmail.com>
2022-10-28 10:53:06 +0200

    refactor

--- mdos-cli/infra/install-linux-mac.sh
+++ mdos-cli/infra/install-linux-mac.sh
@@ -5,11 +5,11 @@
 
     echo "This will install the MDOS CLI onto this machine."
 
-    if [ "$EUID" -ne 0 ]; then
-      echo "This script requires superuser access."
-      echo "Please run it as root or using sudo."
-      exit 1
-    fi
+    # if [ "$EUID" -ne 0 ]; then
+    #   echo "This script requires superuser access."
+    #   echo "Please run it as root or using sudo."
+    #   exit 1
+    # fi
 
     if [ ! -z $1 ]; then
         CLI_VERSION=$1
