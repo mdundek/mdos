34e213cd1397313f3351c3d0e6a8516d51479d77
Michael Dundek <mdundek@gmail.com>
2022-11-16 15:03:29 +0100

    Bumped project up to version 1.5.6

--- mdos-cli/infra/install-linux-mac.sh
+++ mdos-cli/infra/install-linux-mac.sh
@@ -14,7 +14,7 @@
     if [ ! -z $1 ]; then
         CLI_VERSION=$1
     else
-        CLI_VERSION=v1.5.5
+        CLI_VERSION=v1.5.6
     fi
 
     if [[ ! ":$PATH:" == *":/usr/local/bin:"* ]]; then
